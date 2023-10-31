fcfe191232004aecba49be11a53f3b9d129f6698
tekimbaev1 <tekimbaev@gmail.com>
2023-10-31 20:25:14 +0600

    some changes

--- bin/dart_application_3.dart
+++ bin/dart_application_3.dart
@@ -1,25 +1,35 @@
 void main() {
+  //zadacha 1
   String name = 'John';
   print(name.length);
+  //zadacha 2
 
   String age = '25';
   print(int.parse(age) + 5);
 
+  //zadacha 3
   String price = '9.99';
   print(double.parse(price) * 2);
+
+  //zadacha 4
 
   List<String> list = ["apple", "banana", "cherry", "date"];
   print(list.join(', '));
 
+  //zadacha 5
   String message = "Hello, Dart!";
   print(message.contains('"Dart"'));
+
+  //zadacha 6
 
   String sentence = "Dart is a programming language.";
   print(sentence.split(' '));
 
+  //zadacha 7
   String str = '123';
   print(str.isEmpty);
 
+  //zadacha 8
   String number = '42';
   print(int.parse(number).isEven);
 }
